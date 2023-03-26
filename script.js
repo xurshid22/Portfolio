a2a25cc489db295b786809415906f97a2503ea44
Boy <rayimovxurshid248@gmail.com>
2023-03-26 08:56:37 +0500

    First commit, added contact and portfolio.html

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Coder", "YouTuber", "Blogger"],
+    typeSpeed: 100,
+    BackSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
